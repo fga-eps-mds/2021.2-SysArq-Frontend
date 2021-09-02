4fc5b8d33f2ef13f7e4090c0d9a84dbfbaa8990b
Matheus <matheusr.unb@gmail.com>
2021-09-02 06:02:55 -0300

    Aplica Prettier

--- sysarq/src/index.test.js
+++ sysarq/src/index.test.js
@@ -1,24 +1,22 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from "./App"
+import App from "./App";
 
-
-jest.mock("react-dom", () => ({ render: jest.fn() }))
+jest.mock("react-dom", () => ({ render: jest.fn() }));
 
 test("renders with React.StrictMode, App and root div", () => {
+	const root = document.createElement("div");
 
-    const root = document.createElement("div")
+	root.id = "root";
+	document.body.appendChild(root);
 
-    root.id = "root"
-    document.body.appendChild(root)
+	require("./index.js");
 
-    require("./index.js")
-
-    expect(ReactDOM.render).toHaveBeenCalledWith(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>, root
-    )
-
-})
+	expect(ReactDOM.render).toHaveBeenCalledWith(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+		root
+	);
+});
