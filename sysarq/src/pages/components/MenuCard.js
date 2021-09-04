9f6c1ba4dedf42204bcf1fbef438172b45f14ff8
Gabriel <gabrielmr356@hotmail.com>
2021-09-04 17:37:03 -0300

    Foi adicionado o componente menu card

---
+++ sysarq/src/pages/components/MenuCard.js
@@ -0,0 +1,104 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import AllInboxIcon from "@material-ui/icons/AllInbox";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+import CardActionArea from "@material-ui/core/CardActionArea";
+
+// import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+// import SubjectIcon from '@material-ui/icons/Subject';
+// import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+
+
+const useStyles = makeStyles(() => ({
+	card: {
+		height: "100%",
+		display: "flex",
+		flexDirection: "column",
+	},
+
+	icon: {
+		height: 125,
+		color: "#074f66",
+		width: "100%",
+		alignSelf: "center",
+	},
+
+	button: {
+		width: "100%",
+		alignItems: "center",
+		alignSelf: "end",
+	},
+
+	content: {
+		flexGrow: 1,
+	},
+}));
+
+const Icon = ({ iconName }) => {
+	const classes = useStyles();
+
+	let icon = "";
+
+	if (iconName === "icone-assunto") {
+		icon = <AllInboxIcon className={classes.icon} />;
+	}
+	return icon;
+};
+
+const MenuCard = ({ icon, title, createUrl, listUrl }) => {
+	const classes = useStyles();
+
+	return (
+		<Card className={classes.card}>
+			<CardActionArea className={classes.content} href={listUrl}>
+				<CardContent>
+					<Icon iconName={icon} />
+					<Typography
+						align="center"
+						fontFamily="Monospace"
+						gutterBottom
+						variant="h6"
+						component="h2"
+					>
+						{title}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button
+					className={classes.button}
+					href={createUrl}
+					size="small"
+					color="primary"
+				>
+					Criar
+				</Button>
+				<Button
+					className={classes.button}
+					href={listUrl}
+					size="small"
+					color="primary"
+				>
+					Listar
+				</Button>
+			</CardActions>
+		</Card>
+	);
+};
+
+MenuCard.propTypes = {
+	icon: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	createUrl: PropTypes.string.isRequired,
+	listUrl: PropTypes.string.isRequired,
+};
+
+export default MenuCard;
