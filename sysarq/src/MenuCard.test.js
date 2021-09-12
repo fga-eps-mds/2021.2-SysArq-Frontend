f542cc8083d5b4be4c9c84fadfb9b6d786e4004e
Gabriel <gabrielmr356@hotmail.com>
2021-09-11 23:29:01 -0300

    Finalização dos testes da US2

--- sysarq/src/MenuCard.test.js
+++ sysarq/src/MenuCard.test.js
@@ -3,7 +3,6 @@ import { render, screen, fireEvent } from "@testing-library/react";
 
 import MenuCard from "./pages/components/MenuCard";
 
-
 const renderMenuCard = (icon, title, createUrl, listUrl) => {
 	render(
 		<MenuCard
@@ -12,8 +11,8 @@ const renderMenuCard = (icon, title, createUrl, listUrl) => {
 			createUrl={createUrl}
 			listUrl={listUrl}
 		/>
-	)
-}
+	);
+};
 
 const testClick = (testText) => {
 	const clickCreate = screen.getByTestId("click-criar");
@@ -23,63 +22,82 @@ const testClick = (testText) => {
 	const clickList = screen.getByTestId("click-listar");
 	expect(fireEvent.click(clickList)).toBe(true);
 	expect(screen.getByText(testText)).toBeInTheDocument();
-
-}
+};
 
 describe("Button test", () => {
 	it("Document subject", () => {
-
-		renderMenuCard("icone-assunto", "Assunto do Documento",
-		"/create-document-subject", "/fields-register/document-subject")
+		renderMenuCard(
+			"icone-assunto",
+			"Assunto do Documento",
+			"/create-document-subject",
+			"/fields-register/document-subject"
+		);
 
 		testClick("Assunto do Documento");
 	});
 
 	it("Box abbreviation", () => {
-
-		renderMenuCard("icone-sigla-da-caixa", "Sigla da Caixa",
-		"/create-document-subject", "/fields-register/document-subject")
+		renderMenuCard(
+			"icone-sigla-da-caixa",
+			"Sigla da Caixa",
+			"/create-document-subject",
+			"/fields-register/document-subject"
+		);
 
 		testClick("Sigla da Caixa");
-
 	});
 
 	it("Publick worker", () => {
-
-		renderMenuCard("icone-servidor", "Servidor",
-		"/create-document-subject", "/fields-register/document-subject")
+		renderMenuCard(
+			"icone-servidor",
+			"Servidor",
+			"/create-document-subject",
+			"/fields-register/document-subject"
+		);
 
 		testClick("Servidor");
 	});
 
 	it("Unity", () => {
-		
-		renderMenuCard("icone-unidade", "Unidade",
-		"/create-document-subject", "/fields-register/document-subject")
+		renderMenuCard(
+			"icone-unidade",
+			"Unidade",
+			"/create-document-subject",
+			"/fields-register/document-subject"
+		);
 
 		testClick("Unidade");
 	});
 
 	it("Document type", () => {
-
-		renderMenuCard("icone-tipo-de-documento",
-		"Tipo de Documento", "/create-document-subject", "/fields-register/document-subject")
+		renderMenuCard(
+			"icone-tipo-de-documento",
+			"Tipo de Documento",
+			"/create-document-subject",
+			"/fields-register/document-subject"
+		);
 
 		testClick("Tipo de Documento");
 	});
 
 	it("Status", () => {
-		
-		renderMenuCard("icone-status", "Status",
-		"/create-document-subject", "/fields-register/document-subject")
+		renderMenuCard(
+			"icone-status",
+			"Status",
+			"/create-document-subject",
+			"/fields-register/document-subject"
+		);
 
 		testClick("Status");
 	});
 
 	it("Shelf", () => {
-		
-		renderMenuCard("icone-estante-prateleira",
-		"Estante e Prateleira", "/create-document-subject", "/fields-register/document-subject")
+		renderMenuCard(
+			"icone-estante-prateleira",
+			"Estante e Prateleira",
+			"/create-document-subject",
+			"/fields-register/document-subject"
+		);
 
 		testClick("Estante e Prateleira");
 	});
