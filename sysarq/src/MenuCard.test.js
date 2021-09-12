0fe906205f91fa2f4bdd8b9832e244072ef6d91f
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2021-09-12 01:04:34 -0300

    lint e format passando

--- sysarq/src/MenuCard.test.js
+++ sysarq/src/MenuCard.test.js
@@ -29,56 +29,55 @@ const obj = [
 		icon: "icone-assunto",
 		title: "Assunto do Documento",
 		createUrl: "/create-document-subject",
-		listUrl: "/fields-register/document-subject"
+		listUrl: "/fields-register/document-subject",
 	},
 	{
 		icon: "icone-sigla-da-caixa",
 		title: "Sigla da Caixa",
 		createUrl: "/create-box-abbreviation",
-		listUrl:"/fields-register/box-abbreviation"
+		listUrl: "/fields-register/box-abbreviation",
 	},
 	{
-		icon:"icone-servidor",
-		title:"Servidor",
-		createUrl:"/create-public-worker",
-		listUrl:"/fields-register/public-worker"
+		icon: "icone-servidor",
+		title: "Servidor",
+		createUrl: "/create-public-worker",
+		listUrl: "/fields-register/public-worker",
 	},
 	{
-		icon:"icone-unidade",
-		title:"Unidade",
-		createUrl:"/create-unity",
-		listUrl:"/fields-register/unity"
+		icon: "icone-unidade",
+		title: "Unidade",
+		createUrl: "/create-unity",
+		listUrl: "/fields-register/unity",
 	},
 	{
-		icon:"icone-tipo-de-documento",
-		title:"Tipo de Documento",
-		createUrl:"/create-document-type",
-		listUrl:"/fields-register/document-type"
+		icon: "icone-tipo-de-documento",
+		title: "Tipo de Documento",
+		createUrl: "/create-document-type",
+		listUrl: "/fields-register/document-type",
 	},
 	{
-		icon:"icone-status",
-		title:"Status",
-		createUrl:"/create-status",
-		listUrl:"/fields-register/status"
+		icon: "icone-status",
+		title: "Status",
+		createUrl: "/create-status",
+		listUrl: "/fields-register/status",
 	},
 	{
-		icon:"icone-estante-prateleira",
-		title:"Estante e Prateleira",
-		createUrl:"/create-shelf",
-		listUrl:"/fields-register/shelf"
-	}
-]
+		icon: "icone-estante-prateleira",
+		title: "Estante e Prateleira",
+		createUrl: "/create-shelf",
+		listUrl: "/fields-register/shelf",
+	},
+];
 describe("Button test", () => {
-
-	obj.forEach(element => {
+	obj.forEach((element) => {
 		it(element.icon, () => {
 			renderMenuCard(
 				element.icon,
 				element.title,
 				element.createUrl,
 				element.listUrl
-			)
+			);
 			testClick(element.title);
-		})
+		});
 	});
 });
