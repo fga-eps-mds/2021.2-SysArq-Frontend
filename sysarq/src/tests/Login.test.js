f46c8cd49c54b830b501e5edb4220b21a3ffa909
Matheus <matheusr.unb@gmail.com>
2021-09-19 02:21:08 -0300

    Atualiza os testes

--- sysarq/src/tests/Login.test.js
+++ sysarq/src/tests/Login.test.js
@@ -1,169 +1,136 @@
-import React, { Component } from "react";
-import { render, screen, fireEvent, act } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import Login from "../pages/Login/index";
-import MockAdapter from "axios-mock-adapter"
-import { axiosProfile } from "../Api";
+
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 
 
-describe("Main component", () => {
-    it("Show page title", () => {
+const hostApi = `${process.env.REACT_APP_URL_API_PROFILE}api/token/`;
+
+const server = setupServer(
+	rest.post(hostApi, (req, res, ctx) => {
+        if (req.body.username === "teste201") {
+            return res(
+                ctx.status(201)
+            )
+        } else if (req.body.username === "teste401"){
+            return res(
+                ctx.status(401)
+            )
+        } else {
+            return res(
+                ctx.status(404)
+            )
+        }
+    })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("Login Screen Test", () => {
+    it("showPass button test", () => {
         render(<Login />);
 
-        expect(screen.getByText("Entrar")).toBeInTheDocument();
-    });
+        const usernameInput = screen.getByLabelText("Nome de Usuário");
+        fireEvent.change(usernameInput, { target: { value: "teste" } });
 
-    it("Test input fields", () => {
-        render(<Login />);
+        const usernameValue = screen.getByLabelText("Nome de Usuário").value;
+        expect(usernameValue === "teste").toBe(true);
 
-        const inputLogin = screen.getByLabelText(/Nome/);
-        fireEvent.change(inputLogin, { target: { value: "teste" } });
-        const valueLogin = screen.getByLabelText("Nome de Usuário").value;
-        expect(valueLogin === "teste").toBe(true);
-        expect(valueLogin.length === 5).toBe(true);
-
-        const inputPassword = screen.getByLabelText("Senha");
-        fireEvent.change(inputPassword, { target: { value: "teste123" } });
-        const valuePassword = screen.getByLabelText("Senha");
-        expect(valuePassword.value === "teste123").toBe(true);
-        expect(valuePassword.type === "password").toBe(true);
+        const passwordInput = screen.getByLabelText("Senha");
+        fireEvent.change(passwordInput, { target: { value: "teste123" } });
+        
+        const password = screen.getByLabelText("Senha");
+        expect(password.value === "teste123").toBe(true);
+        expect(password.type === "password").toBe(true);
 
         const clickShow = screen.getByTestId("showPass");
         expect(fireEvent.mouseDown(clickShow)).toBe(false);
         expect(fireEvent.click(clickShow)).toBe(true);
 
-
-        const clickSend = screen.getByTestId("send");
-        expect(fireEvent.click(clickSend)).toBe(true);
-    })
-});
-
-    describe("validation", () => {
-        it("Test login fields", () => {
-            render(<Login />);
-
-            const inputLogin = screen.getByLabelText("Nome de Usuário");
-            fireEvent.change(inputLogin, { target: { value: "te" } });
-            const valueLogin = screen.getByLabelText("Nome de Usuário").value;
-            expect(valueLogin === "te").toBe(true);
-            expect(valueLogin.length === 2).toBe(true);
-
-            const clickSend = screen.getByTestId("send");
-            expect(fireEvent.click(clickSend)).toBe(true);
-
-            expect(screen.getByText("Digite um nome de usuário válido"))
-
-        })
-
-        it("Test password fields", () => {
-            render(<Login />);
-
-            const inputLogin = screen.getByLabelText("Nome de Usuário");
-            fireEvent.change(inputLogin, { target: { value: "teste" } });
-            const valueLogin = screen.getByLabelText("Nome de Usuário").value;
-            expect(valueLogin === "teste").toBe(true);
-            expect(valueLogin.length === 5).toBe(true);
-
-            const inputPassword = screen.getByLabelText("Senha");
-            fireEvent.change(inputPassword, { target: { value: "teste" } });
-            const valuePassword = screen.getByLabelText("Senha");
-            expect(valuePassword.value === "teste").toBe(true);
-            expect(valuePassword.type === "password").toBe(true);
-
-            const clickSend = screen.getByTestId("send");
-            expect(fireEvent.click(clickSend)).toBe(true);
-            expect(screen.getByText("Digite uma senha válida"))
-            // screen.debug()
-
-        })
-        
-
+        const visiblePassword = screen.getByLabelText("Senha");
+        expect(visiblePassword.type === "text").toBe(true);
     });
-const hostApi = `${process.env.REACT_APP_URL_API_PROFILE}api/token/`;
 
-describe("axios test", () => {
-    it("Save button", () => {
-        const mock = new MockAdapter(axiosProfile);
-        
+    it("test username field validation", () => {
+        render(<Login />);
+
+        const usernameInput = screen.getByLabelText("Nome de Usuário");
+        fireEvent.change(usernameInput, { target: { value: "te" } });
+
+        const usernameValue = screen.getByLabelText("Nome de Usuário").value;
+        expect(usernameValue === "te").toBe(true);
+
+        const clickLogIn = screen.getByRole("button", { name: /Entrar/ });
+        expect(fireEvent.click(clickLogIn)).toBe(true);
+
+        expect(screen.getByText("Digite um nome de usuário válido"));
+    });
+
+    it("test password field validation", () => {
+        render(<Login />);
+
+        const usernameInput = screen.getByLabelText("Nome de Usuário");
+        fireEvent.change(usernameInput, { target: { value: "teste" } });
+
+        const usernameValue = screen.getByLabelText("Nome de Usuário").value;
+        expect(usernameValue === "teste").toBe(true);
+
+        const passwordInput = screen.getByLabelText("Senha");
+        fireEvent.change(passwordInput, { target: { value: "teste" } });
+
+        const password = screen.getByLabelText("Senha");
+        expect(password.value === "teste").toBe(true);
+
+        const clickLogIn = screen.getByRole("button", { name: /Entrar/ });
+        expect(fireEvent.click(clickLogIn)).toBe(true);
+
+        expect(screen.getByText("Digite uma senha válida"));
+    });
+
+	it("axios success", async () => {
         render(<Login/>);
-        act(()=>{
 
-            const inputLogin = screen.getByLabelText("Nome de Usuário");
-            fireEvent.change(inputLogin, { target: { value: "teste" } });
-            
-            const inputPassword = screen.getByLabelText("Senha");
-            fireEvent.change(inputPassword, { target: { value: "teste123" } });
-        })
+        const usernameInput = screen.getByLabelText("Nome de Usuário");
+        fireEvent.change(usernameInput, { target: { value: "teste201" } });
+        
+        const passwordInput = screen.getByLabelText("Senha");
+        fireEvent.change(passwordInput, { target: { value: "teste201" } });
 
-        
-        const click = screen.getByTestId("send");
-        expect(fireEvent.click(click)).toBe(true);
-        
-        mock.onPost(hostApi).reply(function () {
-            return [200];
-        });
-        
-        expect(mock.history.post.length).toBe(1);
-        expect(mock.history.post[0].data).toBe(
-            JSON.stringify({ username: "teste", password: "teste123" })
-        );
-    });
+        const clickLogIn = screen.getByRole("button", { name: /Entrar/ });
+        expect(fireEvent.click(clickLogIn)).toBe(true);
+	});
 
-    it("error catch", () => {
-        const mock = new MockAdapter(axiosProfile);
-        
+	it("axios error 404", async () => {
         render(<Login/>);
-        act(()=>{
 
-            const inputLogin = screen.getByLabelText("Nome de Usuário");
-            fireEvent.change(inputLogin, { target: { value: "testando" } });
-            
-            const inputPassword = screen.getByLabelText("Senha");
-            fireEvent.change(inputPassword, { target: { value: "testa123" } });
-        })
-
+        const usernameInput = screen.getByLabelText("Nome de Usuário");
+        fireEvent.change(usernameInput, { target: { value: "teste" } });
         
-        // const click = screen.getByTestId("send");
-        // expect(fireEvent.click(click)).toBe(true);
-        
-        // mock.onPost(hostApi).replyOnce(401)
-        // console.log(mock)
-        
-        // expect(mock.history.post.length).toBe(1);
-        // expect(mock.history.post[0].data).toBe(
-        //     JSON.stringify({ username: "testando", password: "testa123" })
-        // );
-    });
-    it('testing the api post method', async () => {
-        const ads = jest.spyOn(axiosProfile, 'post').mockRejectedValue(new Error('error'));
-        await console.log(ads.mockRejectedValue)
-        // await expect(post()).rejects.toThrow('error');  // Success!
-    });
-});
+        const passwordInput = screen.getByLabelText("Senha");
+        fireEvent.change(passwordInput, { target: { value: "teste123" } });
 
-let mock;
+        const clickLogIn = screen.getByRole("button", { name: /Entrar/ });
+        expect(fireEvent.click(clickLogIn)).toBe(true);
 
-beforeAll(() => {
-    mock = new MockAdapter(axiosProfile);
-});
+        await screen.findByText("Erro de conexão!");
+	});
 
-afterEach(() => {
-    mock.reset();
-});
-
-describe("when API call fails", () => {
-    it("should return empty users list", async () => {
+	it("axios error 401", async () => {
         render(<Login/>);
-        // given
-        mock.onPost(`${axiosProfile}api/token/`).networkErrorOnce();
 
-        // when
-        // const result = await fetchUsers();
+        const usernameInput = screen.getByLabelText("Nome de Usuário");
+        fireEvent.change(usernameInput, { target: { value: "teste401" } });
+        
+        const passwordInput = screen.getByLabelText("Senha");
+        fireEvent.change(passwordInput, { target: { value: "teste123" } });
 
-        // then
-        // console.log(mock)
-        // expect(mock.history.post[0].url).toEqual(${ BASE_URL } / users);
-        // expect(result).toEqual([]);
-    });
+        const clickLogIn = screen.getByRole("button", { name: /Entrar/ });
+        expect(fireEvent.click(clickLogIn)).toBe(true);
+
+        await screen.findByText("Nome de Usuário e/ou Senha incorreto!");
+	});
 });
-
