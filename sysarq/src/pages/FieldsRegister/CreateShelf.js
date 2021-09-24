1cc7501c66d9528259a29d6e040c14e3bfc5965d
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2021-09-24 19:27:34 -0300

    Adiciona CreateShelf e seus testes, ainda falta validação dos campos das paginas de create

--- sysarq/src/pages/FieldsRegister/CreateShelf.js
+++ sysarq/src/pages/FieldsRegister/CreateShelf.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Alert from "@material-ui/lab/Alert";
 import Paper from "@material-ui/core/Paper";
+import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import { axiosArchives } from "../../Api";
@@ -12,6 +13,7 @@ export default function CreateShelf() {
 			height: 36,
 			marginBottom: "1rem",
 			maxWidth: 908,
+			marginTop: "1rem",
 		},
 		inputDate: {
 			width: "100%",
@@ -23,6 +25,7 @@ export default function CreateShelf() {
 	});
 	const classes = useStyles();
 
+	const [type, setType] = useState("Estante");
 	const [numberE, setNumberE] = useState(0);
 	const [numberP, setNumberP] = useState(0);
 
@@ -34,34 +37,39 @@ export default function CreateShelf() {
 	const handleCloseError = () => setShowError(false);
 	const handleShowError = () => setShowError(true);
 
-	const onClick = () => {
-		axiosArchives
-			.post(`shelf`, {
-				shelfe_number: numberE,
-				shelfp_number: numberP,
-			})
-			.then(() => {
-				handleShow();
-				setTimeout(handleClose, 3000);
-			})
-			.catch(() => {
-				handleShowError();
-				setTimeout(handleCloseError, 3000);
-			});
+	const handleValueChange = (event) => {
+		setType(event.target.value);
 	};
 
-	const [fields] = useState([
-		{
-			type: "number",
-			placeholder: "Estante",
-			setState: setNumberE,
-		},
-		{
-			type: "number",
-			placeholder: "Prateleira",
-			setState: setNumberP,
-		},
-	]);
+	const onClick = () => {
+		if (type === "Estante") {
+			axiosArchives
+				.post(`shelf/`, {
+					number: numberE,
+				})
+				.then(() => {
+					handleShow();
+					setTimeout(handleClose, 3000);
+				})
+				.catch(() => {
+					handleShowError();
+					setTimeout(handleCloseError, 3000);
+				});
+		} else {
+			axiosArchives
+				.post(`rack/`, {
+					number: numberP,
+				})
+				.then(() => {
+					handleShow();
+					setTimeout(handleClose, 3000);
+				})
+				.catch(() => {
+					handleShowError();
+					setTimeout(handleCloseError, 3000);
+				});
+		}
+	};
 
 	const title = "Arquivo Geral da Policia Civil de Goiás";
 	const subtitle = "Cadastrar estantes e prateleiras";
@@ -78,20 +86,43 @@ export default function CreateShelf() {
 				<h1>{title}</h1>
 				<h2>{subtitle}</h2>
 				<div className="inputs-container">
-					{fields.map((item, key) => {
-						const input = (
-							<TextField
-								key={key.toString()}
-								id={item.placeholder}
-								label={item.placeholder}
-								type={item.type}
-								onChange={({ target }) => item.setState(target.value)}
-								className={classes.input}
-								inputProps={{ maxLength: "100" }}
-							/>
-						);
-						return input;
-					})}
+					<FormControl fullWidth>
+						<InputLabel id="select-shelf-rack-label">Selecione</InputLabel>
+						<Select
+							style={{ textAlign: "left" }}
+							labelId="select-shelf-rack-label"
+							id="select-shelf"
+							value={type}
+							onChange={handleValueChange}
+							renderValue={(value) => `${value}`}
+						>
+							<MenuItem key={0} value="Estante">
+								Estante
+							</MenuItem>
+							<MenuItem key={1} value="Prateleira">
+								Prateleira
+							</MenuItem>
+						</Select>
+					</FormControl>
+					{type === "Estante" ? (
+						<TextField
+							key={0}
+							id="Estante"
+							label="Número da estante"
+							type="number"
+							onChange={({ target }) => setNumberE(target.value)}
+							className={classes.input}
+						/>
+					) : (
+						<TextField
+							key={1}
+							id="Prateleira"
+							label="Número da prateleira"
+							type="number"
+							onChange={({ target }) => setNumberP(target.value)}
+							className={classes.input}
+						/>
+					)}
 				</div>
 				<button data-testid="click" type="button" onClick={onClick}>
 					CADASTRAR
--- sysarq/src/tests/CreateShelf.test.js
+++ sysarq/src/tests/CreateShelf.test.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { render, screen, fireEvent, act } from "@testing-library/react";
+import { render, screen, fireEvent, act, within } from "@testing-library/react";
 
 import CreateShelf from "../pages/FieldsRegister/CreateShelf";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-const axiosArchives = `${process.env.REACT_APP_URL_API_ARCHIVES}shelf/`;
+const axiosArchives = `${process.env.REACT_APP_URL_API_ARCHIVES}`;
 
 const server = setupServer(
-	rest.post(axiosArchives, (req, res, ctx) => {
-		if (req.body.shelfe_number === "201") {
+	rest.post(axiosArchives + `shelf/`, (req, res, ctx) => {
+		if (req.body.number === "201") {
+			return res(ctx.status(201));
+		} else {
+			return res(ctx.status(404));
+		}
+	}),
+	rest.post(axiosArchives + `rack/`, (req, res, ctx) => {
+		if (req.body.number === "201") {
 			return res(ctx.status(201));
 		} else {
 			return res(ctx.status(404));
@@ -28,6 +35,11 @@ const inputChange = (title, targetValue) => {
 		target: { value: targetValue },
 	});
 };
+const selectValue = (title) => {
+	fireEvent.mouseDown(screen.getByLabelText("Selecione"));
+	const subjectsOptions = within(screen.getByRole("listbox"));
+	fireEvent.click(subjectsOptions.getByText(title));
+};
 describe("Main component", () => {
 	it("Show page title", () => {
 		render(<CreateShelf />);
@@ -39,27 +51,45 @@ describe("Main component", () => {
 });
 
 describe("inputs", () => {
-	it("axios sucess", async () => {
+	it("axios sucess rack", async () => {
 		render(<CreateShelf />);
 
-		inputChange("Estante", "201");
-		inputChange("Prateleira", "12");
-
+		selectValue("Prateleira");
+		inputChange("Número da prateleira", "201");
 		fireEvent.click(screen.getByTestId("click"));
-
 		await screen.findByText("Campo cadastrado!");
 		act(() => {
 			jest.advanceTimersByTime(3000);
 		});
 	});
-	it("axios fail", async () => {
+	it("axios sucess shelf", async () => {
 		render(<CreateShelf />);
 
-		inputChange("Estante", "401");
-		inputChange("Prateleira", "12");
-
+		selectValue("Estante");
+		inputChange("Número da estante", "201");
 		fireEvent.click(screen.getByTestId("click"));
+		await screen.findByText("Campo cadastrado!");
+		act(() => {
+			jest.advanceTimersByTime(3000);
+		});
+	});
+	it("axios fail rack", async () => {
+		render(<CreateShelf />);
 
+		selectValue("Prateleira");
+		inputChange("Número da prateleira", "401");
+		fireEvent.click(screen.getByTestId("click"));
+		await screen.findByText("Erro de conexão!");
+		act(() => {
+			jest.advanceTimersByTime(3000);
+		});
+	});
+	it("axios fail shelf", async () => {
+		render(<CreateShelf />);
+
+		selectValue("Estante");
+		inputChange("Número da estante", "401");
+		fireEvent.click(screen.getByTestId("click"));
 		await screen.findByText("Erro de conexão!");
 		act(() => {
 			jest.advanceTimersByTime(3000);
