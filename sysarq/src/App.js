25321605015e1d06cca616643a366e688dbe5dd2
Matheus <matheusr.unb@gmail.com>
2021-09-26 02:00:48 -0300

    Atualiza Api.js

--- sysarq/src/App.js
+++ sysarq/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 import Home from "./pages/Home";
