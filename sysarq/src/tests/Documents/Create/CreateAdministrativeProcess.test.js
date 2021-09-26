7b5391177f125ed6c010ed52b0c617687cd6703f
Matheus <matheusr.unb@gmail.com>
2021-09-26 20:28:36 -0300

    Complementa testes e realiza correções

--- sysarq/src/tests/Documents/Create/CreateAdministrativeProcess.test.js
+++ sysarq/src/tests/Documents/Create/CreateAdministrativeProcess.test.js
@@ -116,6 +116,8 @@ const server = setupServer(
 			req.body.is_filed === false &&
 			req.body.is_eliminated === false &&
 			req.body.unity_id === 41 &&
+			req.body.administrative_process_number === "50" &&
+			req.body.send_date === "2055-09-08" &&
 			req.body.notes === "notes_test"
 		) {
 			return res(ctx.status(201));
@@ -145,33 +147,46 @@ const isNotInTheDocument = (text) => {
 	expect(screen.queryByText(text)).not.toBeInTheDocument();
 };
 
+const NOTICE_DATE_LABEL = "Data de Autuação*";
+
+const INVALID_DATE_ERROR_MESSAGE = "Insira uma data válida";
+
+const ARCHIVING_DATE_LABEL = "Data de Arquivamento*";
+
+const UNARCHIVE_DESTINATION_UNIT_LABEL =
+	"Unidade de Destino do Desarquivamento";
+
+const UNARCHIVE_PROCESS_NUMBER_LABEL = "Número do Processo do Desarquivamento";
+
+const UNARCHIVE_DATE_LABEL = "Data de Desarquivamento";
+
 describe("Create Administrative Process Screen Test", () => {
-	it("t", async () => {
+	it("complete test", async () => {
 		render(<CreateAdministrativeProcess />);
 
-		input("Data de Autuação*", "");
+		input(NOTICE_DATE_LABEL, "");
 		submitClick();
 		isOnTheScreen("Insira a data de autuação");
 
-		input("Data de Autuação*", "01/02/");
+		input(NOTICE_DATE_LABEL, "01/02/");
 		isNotInTheDocument("Insira a data de autuação");
 		submitClick();
-		isOnTheScreen("Insira uma data válida");
+		isOnTheScreen(INVALID_DATE_ERROR_MESSAGE);
 
-		input("Data de Autuação*", "03/04/2005");
-		isNotInTheDocument("Insira uma data válida");
+		input(NOTICE_DATE_LABEL, "03/04/2005");
+		isNotInTheDocument(INVALID_DATE_ERROR_MESSAGE);
 
-		input("Data de Arquivamento*", "");
+		input(ARCHIVING_DATE_LABEL, "");
 		submitClick();
 		isOnTheScreen("Insira a data de arquivamento");
 
-		input("Data de Arquivamento*", "36/07/2008");
+		input(ARCHIVING_DATE_LABEL, "36/07/2008");
 		isNotInTheDocument("Insira a data de arquivamento");
 		submitClick();
-		isOnTheScreen("Insira uma data válida");
+		isOnTheScreen(INVALID_DATE_ERROR_MESSAGE);
 
-		input("Data de Arquivamento*", "09/10/2011");
-		isNotInTheDocument("Insira uma data válida");
+		input(ARCHIVING_DATE_LABEL, "09/10/2011");
+		isNotInTheDocument(INVALID_DATE_ERROR_MESSAGE);
 
 		input("Referência", "13/2012");
 		submitClick();
@@ -235,6 +250,12 @@ describe("Create Administrative Process Screen Test", () => {
 			/Verifique sua conexão com a internet e recarregue a página./i
 		);
 
+		fireEvent.mouseDown(screen.getByLabelText("Status*"));
+		const statusOptions1 = within(screen.getByRole("listbox"));
+		fireEvent.click(statusOptions1.getByText("Arquivado"));
+
+		submitClick();
+
 		fireEvent.mouseDown(screen.getByLabelText("Unidade de Destino"));
 		const destinationUnitOptions = within(screen.getByRole("listbox"));
 		await destinationUnitOptions.findByText("destination_unit_name_test");
@@ -259,284 +280,45 @@ describe("Create Administrative Process Screen Test", () => {
 		await rackOptions.findByText("49");
 		fireEvent.click(rackOptions.getByText(/49/i));
 
-		isNotInTheDocument("Unidade de Destino do Desarquivamento");
-		isNotInTheDocument("Número do Processo do Desarquivamento");
-		isNotInTheDocument("Data de Desarquivamento");
+		isNotInTheDocument(UNARCHIVE_DESTINATION_UNIT_LABEL);
+		isNotInTheDocument(UNARCHIVE_PROCESS_NUMBER_LABEL);
+		isNotInTheDocument(UNARCHIVE_DATE_LABEL);
 
 		fireEvent.mouseDown(screen.getByLabelText("Status*"));
-		const statusOptions1 = within(screen.getByRole("listbox"));
-		fireEvent.click(statusOptions1.getByText(/Desarquivado/i));
+		const statusOptions2 = within(screen.getByRole("listbox"));
+		fireEvent.click(statusOptions2.getByText(/Desarquivado/i));
 
-		isOnTheScreen("Unidade de Destino do Desarquivamento");
-		isOnTheScreen("Número do Processo do Desarquivamento");
-		isOnTheScreen("Data de Desarquivamento");
+		isOnTheScreen(UNARCHIVE_DESTINATION_UNIT_LABEL);
+		isOnTheScreen(UNARCHIVE_PROCESS_NUMBER_LABEL);
+		isOnTheScreen(UNARCHIVE_DATE_LABEL);
 
 		fireEvent.mouseDown(
-			screen.getByLabelText("Unidade de Destino do Desarquivamento")
+			screen.getByLabelText(UNARCHIVE_DESTINATION_UNIT_LABEL)
 		);
+
 		const unarchiveDestinationUnitOptions = within(screen.getByRole("listbox"));
 		await unarchiveDestinationUnitOptions.findByText(
 			"unarchive_unit_name_test"
 		);
+
 		fireEvent.click(
 			unarchiveDestinationUnitOptions.getByText(/unarchive_unit_name_test/i)
 		);
 
-		input("Número do Processo do Desarquivamento", "50")
+		input(UNARCHIVE_PROCESS_NUMBER_LABEL, "50");
 
-		input("Data de Desarquivamento", "51/2052");
+		input(UNARCHIVE_DATE_LABEL, "/06/2052");
 		submitClick();
 		isOnTheScreen("Insira uma data de desarquivamento válida");
+
+		input(UNARCHIVE_DATE_LABEL, "08/09/2055");
+		isNotInTheDocument("Insira uma data de desarquivamento válida");
+
+		input("Observação", "notes_test");
+
+		submitClick();
+
+		const successAlert = await screen.findByRole("alert");
+		expect(successAlert).toHaveTextContent(/Documento cadastrado!/i);
 	});
-
-	// 	fireEvent.change(screen.getByLabelText("Data de Arquivamento*"), {
-	// 		target: { value: "34/05/2006" },
-	// 	});
-	// 	expect(
-	// 		screen.queryByText("Insira a data de arquivamento")
-	// 	).not.toBeInTheDocument();
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(screen.getByText("Insira uma data válida")).toBeInTheDocument();
-
-	// 	fireEvent.change(screen.getByLabelText("Data de Arquivamento*"), {
-	// 		target: { value: "07/08/2009" },
-	// 	});
-	// 	expect(
-	// 		screen.queryByText("Insira uma data válida")
-	// 	).not.toBeInTheDocument();
-	// 	fireEvent.change(screen.getByLabelText("Referência"), {
-	// 		target: { value: "13/2010" },
-	// 	});
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(
-	// 		screen.getByText("Insira uma referência válida")
-	// 	).toBeInTheDocument();
-
-	// 	fireEvent.change(screen.getByLabelText("Referência"), {
-	// 		target: { value: "11/2012" },
-	// 	});
-	// 	expect(
-	// 		screen.queryByText("Insira uma referência válida")
-	// 	).not.toBeInTheDocument();
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(screen.getByText("Insira o número do processo")).toBeInTheDocument();
-
-	// 	fireEvent.change(screen.getByLabelText("Número do Processo*"), {
-	// 		target: { value: "131" },
-	// 	});
-	// 	expect(
-	// 		screen.queryByText("Insira o número do processo")
-	// 	).not.toBeInTheDocument();
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(screen.getByText("Insira um interessado")).toBeInTheDocument();
-
-	// 	fireEvent.change(screen.getByLabelText("Interessado*"), {
-	// 		target: { value: "interested_test" },
-	// 	});
-	// 	expect(screen.queryByText("Insira um interessado")).not.toBeInTheDocument();
-
-	// 	fireEvent.change(screen.getByLabelText("CPF/CNPJ"), {
-	// 		target: { value: "1415." },
-	// 	});
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(screen.getByText("Insira somente números")).toBeInTheDocument();
-
-	// 	fireEvent.change(screen.getByLabelText("CPF/CNPJ"), {
-	// 		target: { value: "1415" },
-	// 	});
-	// 	expect(
-	// 		screen.queryByText("Insira somente números")
-	// 	).not.toBeInTheDocument();
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(screen.getByText("Insira um CPF/CNPJ válido")).toBeInTheDocument();
-
-	// 	fireEvent.change(screen.getByLabelText("CPF/CNPJ"), {
-	// 		target: { value: "16171819202" },
-	// 	});
-	// 	expect(
-	// 		screen.queryByText("Insira um CPF/CNPJ válido")
-	// 	).not.toBeInTheDocument();
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	expect(
-	// 		screen.queryByText("Insira a data de autuação")
-	// 	).not.toBeInTheDocument();
-	// 	expect(
-	// 		screen.queryByText("Insira uma data válida")
-	// 	).not.toBeInTheDocument();
-	// 	expect(
-	// 		screen.queryByText("Insira a data de arquivamento")
-	// 	).not.toBeInTheDocument();
-	// 	expect(
-	// 		screen.queryByText("Insira uma referência válida")
-	// 	).not.toBeInTheDocument();
-	// 	expect(
-	// 		screen.queryByText("Insira o número do processo")
-	// 	).not.toBeInTheDocument();
-	// 	expect(screen.queryByText("Insira um interessado")).not.toBeInTheDocument();
-	// 	expect(
-	// 		screen.queryByText("Insira somente números")
-	// 	).not.toBeInTheDocument();
-	// 	expect(
-	// 		screen.queryByText("Insira um CPF/CNPJ válido")
-	// 	).not.toBeInTheDocument();
-	// });
-
-	// it("select fields input test, error post and post success", async () => {
-	// 	render(<CreateAdministrativeProcess />);
-
-	// 	fireEvent.change(screen.getByLabelText("Data de Autuação*"), {
-	// 		target: { value: "12/05/2026" },
-	// 	});
-	// 	fireEvent.change(screen.getByLabelText("Data de Arquivamento*"), {
-	// 		target: { value: "27/10/2029" },
-	// 	});
-	// 	fireEvent.change(screen.getByLabelText("Referência"), {
-	// 		target: { value: "03/2031" },
-	// 	});
-	// 	fireEvent.change(screen.getByLabelText("Número do Processo*"), {
-	// 		target: { value: "3234" },
-	// 	});
-	// 	fireEvent.change(screen.getByLabelText("CPF/CNPJ"), {
-	// 		target: { value: "35363738394" },
-	// 	});
-	// 	fireEvent.change(screen.getByLabelText("Interessado*"), {
-	// 		target: { value: "interested_test_1" },
-	// 	});
-	// 	fireEvent.change(screen.getByLabelText("Servidor que Encaminhou"), {
-	// 		target: { value: "sender_worker_test" },
-	// 	});
-
-	// 	fireEvent.change(screen.getByLabelText("Observação"), {
-	// 		target: { value: "notes_test_404" },
-	// 	});
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(screen.getByText("Selecione um assunto")).toBeInTheDocument();
-
-	// 	fireEvent.mouseDown(screen.getByLabelText("Assunto do Documento*"));
-	// 	const subjectsOptions = within(screen.getByRole("listbox"));
-	// 	await subjectsOptions.findByText("subject_name_test");
-	// 	fireEvent.click(subjectsOptions.getByText(/subject_name_test/i));
-	// 	expect(screen.queryByText("Selecione um assunto")).not.toBeInTheDocument();
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-	// 	expect(screen.getByText("Selecione um status")).toBeInTheDocument();
-
-	// 	fireEvent.mouseDown(screen.getByLabelText("Status*"));
-	// 	const statusOptions = within(screen.getByRole("listbox"));
-	// 	await statusOptions.findByText("Arquivado");
-	// 	fireEvent.click(statusOptions.getByText("Arquivado"));
-	// 	expect(screen.queryByText("Selecione um status")).not.toBeInTheDocument();
-
-	// 	fireEvent.mouseDown(screen.getByLabelText("Unidade de Destino"));
-	// 	const destUnitOptions = within(screen.getByRole("listbox"));
-	// 	await destUnitOptions.findByText("dest_unit_name_test");
-	// 	fireEvent.click(destUnitOptions.getByText(/dest_unit_name_test/i));
-
-	// 	fireEvent.mouseDown(screen.getByLabelText("Unidade que Encaminhou"));
-	// 	const senderUnitOptions = within(screen.getByRole("listbox"));
-	// 	await senderUnitOptions.findByText("sender_unit_name_test_1");
-	// 	fireEvent.click(senderUnitOptions.getByText(/sender_unit_name_test_1/i));
-
-	// 	fireEvent.mouseDown(screen.getByLabelText("Sigla da Caixa"));
-	// 	const abbreviationOptions = within(screen.getByRole("listbox"));
-	// 	await abbreviationOptions.findByText("abbreviation_test");
-	// 	fireEvent.click(abbreviationOptions.getByText(/abbreviation_test/i));
-
-	// 	fireEvent.mouseDown(screen.getByLabelText("Estante"));
-	// 	const shelfOptions = within(screen.getByRole("listbox"));
-	// 	await shelfOptions.findByText("11");
-	// 	fireEvent.click(shelfOptions.getByText(/11/i));
-
-	// 	fireEvent.mouseDown(screen.getByLabelText("Prateleira"));
-	// 	const rackOptions = within(screen.getByRole("listbox"));
-	// 	await rackOptions.findByText("12");
-	// 	fireEvent.click(rackOptions.getByText(/12/i));
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const errorAlert = await screen.findByRole("alert");
-	// 	expect(errorAlert).toHaveTextContent(
-	// 		/Verifique sua conexão com a internet e recarregue a página./i
-	// 	);
-
-	// 	fireEvent.change(screen.getByLabelText("Observação"), {
-	// 		target: { value: "notes_test" },
-	// 	});
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const successAlert = await screen.findByRole("alert");
-	// 	expect(successAlert).toHaveTextContent(/Documento cadastrado!/i);
-	// });
-
-	// it("document-subject get error", async () => {
-	// 	render(<CreateAdministrativeProcess />);
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const errorAlert = await screen.findByRole("alert");
-	// 	expect(errorAlert).toHaveTextContent(
-	// 		/Verifique sua conexão com a internet e recarregue a página./i
-	// 	);
-	// });
-
-	// it("document-subject get error", async () => {
-	// 	render(<CreateAdministrativeProcess />);
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const errorAlert = await screen.findByRole("alert");
-	// 	expect(errorAlert).toHaveTextContent(
-	// 		/Verifique sua conexão com a internet e recarregue a página./i
-	// 	);
-	// });
-
-	// it("unit get error", async () => {
-	// 	render(<CreateAdministrativeProcess />);
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const errorAlert = await screen.findByRole("alert");
-	// 	expect(errorAlert).toHaveTextContent(
-	// 		/Verifique sua conexão com a internet e recarregue a página./i
-	// 	);
-	// });
-
-	// it("box-abbreviation get error", async () => {
-	// 	render(<CreateAdministrativeProcess />);
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const errorAlert = await screen.findByRole("alert");
-	// 	expect(errorAlert).toHaveTextContent(
-	// 		/Verifique sua conexão com a internet e recarregue a página./i
-	// 	);
-	// });
-
-	// it("shelf get error", async () => {
-	// 	render(<CreateAdministrativeProcess />);
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const errorAlert = await screen.findByRole("alert");
-	// 	expect(errorAlert).toHaveTextContent(
-	// 		/Verifique sua conexão com a internet e recarregue a página./i
-	// 	);
-	// });
-
-	// it("status get error", async () => {
-	// 	render(<CreateAdministrativeProcess />);
-
-	// 	fireEvent.click(screen.getByRole("button", { name: /CADASTRAR/ }));
-
-	// 	const errorAlert = await screen.findByRole("alert");
-	// 	expect(errorAlert).toHaveTextContent(
-	// 		/Verifique sua conexão com a internet e recarregue a página./i
-	// 	);
-	// });
 });
