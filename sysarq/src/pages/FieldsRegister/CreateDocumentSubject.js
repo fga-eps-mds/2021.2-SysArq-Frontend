00ea475e2e14c7a7aa0ee695d8491ab595921d03
darmsDD <ivandinizdobbin2@gmail.com>
2021-09-27 19:12:42 -0300

    Cria form padrão para o CreateDocumentSubject e CreateDocumentType

--- sysarq/src/pages/FieldsRegister/CreateDocumentSubject.js
+++ sysarq/src/pages/FieldsRegister/CreateDocumentSubject.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import Alert from "@material-ui/lab/Alert";
-import { Paper, TextField, Grid, Container } from "@material-ui/core";
+import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { axiosArchives } from "../../Api";
+import createForm from "./form";
 
 const useStyles = makeStyles({
 	input: {
@@ -84,63 +83,5 @@ export default function CreateDocumentSubject() {
 	const title = "Arquivo Geral da Policia Civil de Goiás";
 	const subtitle = "Cadastrar assunto do documento";
 
-	return (
-		<div className="create-form-container">
-			{show === true ? <Alert severity="success">Campo cadastrado!</Alert> : ""}
-			{showError === true ? (
-				<Alert severity="error">Erro de conexão!</Alert>
-			) : (
-				""
-			)}
-			<Paper className="form-cadastro-container" elevation={10}>
-				<h1>{title}</h1>
-				<h2>{subtitle}</h2>
-				<div className="inputs-container">
-					<Container className="container">
-						<Grid container spacing={2}>
-							{fields.map((item, key) => {
-								const input =
-									item.type !== "date" ? (
-										<Grid item xs={12} sm={12} md={12} key={key.toString()}>
-											<TextField
-												id={item.placeholder}
-												label={item.placeholder}
-												type={item.type}
-												value={item.value}
-												onChange={(event) => {
-													item.setValue(event.target.value);
-													item.setHelperText("");
-													item.setError(false);
-												}}
-												className={classes.input}
-												inputProps={{ maxLength: "100" }}
-											/>
-										</Grid>
-									) : (
-										<Grid item xs={12} sm={12} md={12} key={key.toString()}>
-											<TextField
-												id={item.placeholder}
-												label={item.placeholder}
-												type={item.type}
-												value={item.value}
-												onChange={(event) => {
-													item.setValue(event.target.value);
-													item.setHelperText("");
-													item.setError(false);
-												}}
-												className={classes.inputDate}
-											/>
-										</Grid>
-									);
-								return input;
-							})}
-						</Grid>
-					</Container>
-				</div>
-				<button data-testid="click" type="button" onClick={onClick}>
-					CADASTRAR
-				</button>
-			</Paper>
-		</div>
-	);
+	return createForm(fields, title, subtitle, classes, show, showError, onClick);
 }
