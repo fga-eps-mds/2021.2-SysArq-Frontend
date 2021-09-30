6cce0beb476cbefbd89a1d3342f3d13072180836
Matheus <matheusr.unb@gmail.com>
2021-09-30 16:46:01 -0300

    Adiciona e inicia os testes da criação de folha de frequências

---
+++ sysarq/src/tests/Documents/Create/CreateFrequencySheet.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+
+import CreateFrequencySheet from "../../../pages/Documents/Create/CreateFrequencySheet";
+
+describe("Create Frequency Relation Screen Test", () => {
+	it("complete test", async () => {
+		render(<CreateFrequencySheet />);
+	});
+});
