a2e449279c3eef800b87404748a6b3c85399d1bc
Matheus <matheusr.unb@gmail.com>
2021-10-30 02:53:23 -0300

    Atualiza testes

--- sysarq/src/tests/Documents/Create/ConnectionErrors/ShelfConnectionError.test.js
+++ sysarq/src/tests/Documents/Create/ConnectionErrors/ShelfConnectionError.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
-import CreateAdministrativeProcess from "../../../../pages/Documents/Create/CreateAdministrativeProcess";
+import CreateBoxArchiving from "../../../../pages/Documents/Create/CreateBoxArchiving";
 
 import { failedShelfServer } from "../../../support/server";
 
@@ -10,7 +10,7 @@ afterAll(() => failedShelfServer.close());
 
 describe("Shelf Connection Error Test", () => {
 	it("connectionError test", async () => {
-		render(<CreateAdministrativeProcess />);
+		render(<CreateBoxArchiving />);
 
 		const errorAlert = await screen.findByRole("alert");
 		expect(errorAlert).toHaveTextContent(
