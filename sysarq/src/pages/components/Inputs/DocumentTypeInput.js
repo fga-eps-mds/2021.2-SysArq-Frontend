5b72f218727ade25188c158a96504facf458fba9
Matheus <matheusr.unb@gmail.com>
2021-11-04 04:25:47 -0300

    Atualiza componente DocumentTypeInput

--- sysarq/src/pages/components/Inputs/DocumentTypeInput.js
+++ sysarq/src/pages/components/Inputs/DocumentTypeInput.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 import {
 	Grid,
+	TextField,
 	FormControl,
 	InputLabel,
 	Select,
@@ -14,15 +15,15 @@ import { axiosArchives, axiosProfile } from "../../../Api";
 import { logout } from "../../../support";
 
 const DocumentTypeInput = ({
+	isDetailPage,
+	documentTypeDetail,
 	setHelperText,
 	set,
 	connectionError,
 	documentType,
 	documentTypeHelperText,
-	isDisabled,
 }) => {
 	const [documentTypes, setDocumentTypes] = useState([]);
-	const [documentTypeValue, setDocumentTypeValue] = useState("");
 
 	const handleChange = (event) => {
 		setHelperText("");
@@ -37,19 +38,13 @@ const DocumentTypeInput = ({
 			.then((res) => {
 				localStorage.setItem("tk", res.data.access);
 				localStorage.setItem("tkr", res.data.refresh);
+
 				axiosArchives
 					.get("document-type/", {
 						headers: { Authorization: `JWT ${localStorage.getItem("tk")}` },
 					})
 					.then((response) => {
-						setDocumentTypes(response.data)
-						setDocumentTypeValue(documentType);
-						
-						response.data.forEach(document => {
-							if(document.id === documentType) {
-								setDocumentTypeValue(document);
-							}
-						})
+						setDocumentTypes(response.data);
 					})
 					.catch(() => connectionError());
 			})
@@ -62,46 +57,56 @@ const DocumentTypeInput = ({
 
 	return (
 		<Grid item xs={12} sm={12} md={12}>
-			<FormControl fullWidth error={documentTypeHelperText !== ""}>
-				<InputLabel id="select-documentType-label">
-					Tipo de Documento*
-				</InputLabel>
-				<Select
-					style={{ textAlign: "left" }}
-					labelId="select-documentType-label"
-					id="select-documentType"
-					value={documentTypeValue}
-					onChange={handleChange}
-					renderValue={(value) => `${value.document_name}`}
-					disabled={isDisabled}
-				>
-					<MenuItem key={0} value="">
-						<em>Nenhum</em>
-					</MenuItem>
-
-					{documentTypes.map((documentTypeOption) => (
-						<MenuItem key={documentTypeOption.id} value={documentTypeOption}>
-							{documentTypeOption.document_name}
+			{isDetailPage ? (
+				<TextField
+					fullWidth
+					id="documentType"
+					label="Tipo de Documento"
+					value={documentTypeDetail}
+					inputProps={{ readOnly: true }}
+				/>
+			) : (
+				<FormControl fullWidth error={documentTypeHelperText !== ""}>
+					<InputLabel id="select-documentType-label">
+						Tipo de Documento*
+					</InputLabel>
+					<Select
+						style={{ textAlign: "left" }}
+						labelId="select-documentType-label"
+						id="select-documentType"
+						value={documentType}
+						onChange={handleChange}
+						renderValue={(value) => `${value.document_name}`}
+					>
+						<MenuItem key={0} value="">
+							<em>Nenhum</em>
 						</MenuItem>
-					))}
-				</Select>
-				{documentTypeHelperText ? (
-					<FormHelperText>{documentTypeHelperText}</FormHelperText>
-				) : (
-					""
-				)}
-			</FormControl>
+
+						{documentTypes.map((documentTypeOption) => (
+							<MenuItem key={documentTypeOption.id} value={documentTypeOption}>
+								{documentTypeOption.document_name}
+							</MenuItem>
+						))}
+					</Select>
+					{documentTypeHelperText ? (
+						<FormHelperText>{documentTypeHelperText}</FormHelperText>
+					) : (
+						""
+					)}
+				</FormControl>
+			)}
 		</Grid>
 	);
 };
 
 DocumentTypeInput.propTypes = {
+	isDetailPage: PropTypes.bool.isRequired,
+	documentTypeDetail: PropTypes.string.isRequired,
 	setHelperText: PropTypes.func.isRequired,
 	set: PropTypes.func.isRequired,
 	connectionError: PropTypes.func.isRequired,
 	documentType: PropTypes.string.isRequired,
 	documentTypeHelperText: PropTypes.string.isRequired,
-	isDisabled: PropTypes.string.isRequired,
 };
 
 export default DocumentTypeInput;
