67265c1d289383668bbcaa1d6001435123f501da
black258br <87656157+black258br@users.noreply.github.com>
2022-03-07 17:44:33 -0300

    Adicionar máscara do CPF no cadastro de servidor
    Co-authored-by: <Wesley Lira> <wesleeylirac12@gmail.com>
    Co-authored-by: <Arthur Henrique> <arthurhenrir4@gmail.com>

--- sysarq/src/pages/Fields/form.js
+++ sysarq/src/pages/Fields/form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Paper, TextField, Grid, Container } from "@material-ui/core";
+import InputMask from 'react-input-mask';
 import PopUpAlert from "../components/PopUpAlert";
 
 export default function createForm(
@@ -22,6 +23,31 @@ export default function createForm(
 					<Container className="container">
 						<Grid container spacing={2}>
 							{fields.map((item, key) => {
+								if (item.placeholder === "CPF*") {
+									const input = (
+										<Grid item xs={12} sm={12} md={12} key={key.toString()}>
+											<InputMask
+												mask="999.999.999-99"
+												id={item.placeholder}
+												label={item.placeholder}
+												type={item.type}
+												value={item.value}
+												onChange={(event) => {
+													item.setValue(event.target.value);
+													item.setHelperText("");
+													item.setError(false);
+												}}
+												className={classes.input}
+												inputProps={{ maxLength: "100" }}
+												helperText={item.helperText}
+												error={item.error}
+											>
+												<TextField/>
+												</InputMask>
+										</Grid>
+									);
+									return input;
+								}
 								const input = (
 									<Grid item xs={12} sm={12} md={12} key={key.toString()}>
 										<TextField
