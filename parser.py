352dadda332e147cac0d2bd6e4f1c395a8020244
João Luis Baraky <jlbaraky@gmail.com>
2022-03-18 23:26:20 -0300

    Adicionar parser.py e mtrics.yml

---
+++ parser.py
@@ -0,0 +1,37 @@
+import json
+import requests
+import sys
+from datetime import datetime
+
+TODAY = datetime.now()
+
+METRICS_SONAR = [
+    'files',
+    'functions',
+    'complexity',
+    'comment_lines_density',
+    'duplicated_lines_density',
+    'coverage',
+    'ncloc',
+    'tests',
+    'test_errors',
+    'test_failures',
+    'test_execution_time',
+    'security_rating'
+]
+
+BASE_URL = 'https://sonarcloud.io/api/measures/component_tree?component=fga-eps-mds_'
+
+if __name__ == '__main__':
+
+    REPO = sys.argv[1]
+    RELEASE_VERSION = sys.argv[2]
+
+    response = requests.get(f'{BASE_URL}{REPO}&metricKeys={",".join(METRICS_SONAR)}&ps=500')
+    j = json.loads(response.text)
+
+    file_path = f'./analytics-raw-data/fga-eps-mds-{REPO}-{TODAY.strftime("%m-%d-%Y-%H-%M-%S")}-{RELEASE_VERSION}.json'
+
+    with open(file_path, 'w') as fp:
+        fp.write(json.dumps(j))
+        fp.close()
