8b85d7bdca536e38ad17fcc20b7a11e1bff209d2
Weslin-0101 <wesleeylirac12@gmail.com>
2022-04-12 02:21:27 -0300

    refatorando

--- sysarq/src/pages/Fields/Create/CreatePublicWorker.js
+++ sysarq/src/pages/Fields/Create/CreatePublicWorker.js
@@ -39,7 +39,7 @@ export default function CreatePublicWorker() {
 		setOpenAlert(false);
 	};
 
-	const connectionError = () => {
+	const connectionError = (value) => {
 		setOpenAlert(true);
 		setSeverityAlert("error");
 
