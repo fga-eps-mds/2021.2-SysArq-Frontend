3b2c9971e855ac37f2d1de314e63bc62adbcef6e
black258br <87656157+black258br@users.noreply.github.com>
2022-04-19 11:24:06 -0300

    Alinhamento dos cards alterados para vertical
    Co-Authored-By: André Corrêa da Silva <78980992+dartmol203@users.noreply.github.com>

--- sysarq/src/pages/Search/Search.js
+++ sysarq/src/pages/Search/Search.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+
+
 import MenuCard from "../components/MenuCard";
 import CardContainer from "../components/Container/CardContainer";
 
@@ -13,65 +16,67 @@ export default function Search() {
 				<img id="logo" src={imgBox} alt="Logo" />
 				<h1 id="search_title">Arquivo Geral da Polícia Civil de Goiás</h1>
 
-				<CardContainer title="Campos Obrigatórios" spacing={2}>
-					<MenuCard
-						icon="document-name-icon"
-						title="Nome do Documento"
-						url="/fields/document-name"
-						lg={2}
-					/>
-					<MenuCard
-						icon="unit-icon"
-						title="Unidade"
-						url="/fields/unity"
-						lg={2}
-					/>
-					<MenuCard
-						icon="abbreviation-icon"
-						title="Sigla da Caixa"
-						url="/fields/box-abbreviation"
-						lg={2}
-					/>
-					<MenuCard
-						icon="shelf-rack-icon"
-						title="Localidade do Documento"
-						url="/fields/shelf"
-						lg={2}
-					/>
-					<MenuCard
-						icon="public-worker-icon"
-						title="Servidor"
-						url="/fields/public-worker"
-						lg={2}
-					/>
-				</CardContainer>
-
 				<CardContainer title="Documentos" spacing={2}>
 					<MenuCard
 						icon="administrative-process-icon"
 						title="Processo Administrativo"
-						url="/documents/administrative-process"
-						lg={3}
-					/>
+						url="/documents/administrative-process"						
+						lg={10}
+					/>						
 					<MenuCard
 						icon="frequency-relation-icon"
 						title="Relação de Frequências"
 						url="/documents/frequency-relation"
-						lg={3}
+						lg={10}
 					/>
 					<MenuCard
 						icon="frequency-sheet-icon"
 						title="Folha de Frequências"
 						url="/documents/frequency-sheet"
-						lg={3}
+						lg={10}
 					/>
 					<MenuCard
 						icon="box-archiving-icon"
 						title="Arquivamento de Caixas"
 						url="/documents/box-archiving"
-						lg={3}
+						lg={10}
 					/>
 				</CardContainer>
+
+				<CardContainer title="Campos Obrigatórios" spacing={2}>
+					<MenuCard
+						icon="document-name-icon"
+						title="Nome do Documento"
+						url="/fields/document-name"
+						lg={10}
+					/>
+					<MenuCard
+						icon="unit-icon"
+						title="Unidade"
+						url="/fields/unity"
+						lg={10}
+					/>
+					<MenuCard
+						icon="abbreviation-icon"
+						title="Sigla da Caixa"
+						url="/fields/box-abbreviation"
+						lg={10}
+					/>
+					<MenuCard
+						icon="shelf-rack-icon"
+						title="Localidade do Documento"
+						url="/fields/shelf"
+						lg={10}
+					/>
+					<MenuCard
+						icon="public-worker-icon"
+						title="Servidor"
+						url="/fields/public-worker"
+						lg={10}
+					/>
+				</CardContainer>
+
+				
 			</section>
 		</body>
 	);
