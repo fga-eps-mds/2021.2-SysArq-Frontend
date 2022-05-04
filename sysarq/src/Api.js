7630000745dbcb11ecb9fbe27c76697191f4aebe
Paulo Filho <paulomacieltorresfilho@gmail.com>
2022-05-04 01:38:04 -0400

    todos os posts irão para o backend com letras maiúsculas

--- sysarq/src/Api.js
+++ sysarq/src/Api.js
@@ -11,4 +11,26 @@ const axiosArchives = axios.create({
 	baseURL: hostApiArchives,
 });
 
+const objectMap = (obj, fn) =>
+	Object.fromEntries(Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)]));
+
+axiosArchives.interceptors.request.use(
+	(config) => {
+		if (config.data) {
+			config.data = objectMap(config.data, (item) => {
+				if (typeof item === "string" && item !== "") {
+					const newItem = item
+						.split(" ")
+						.map((w) => w.toUpperCase())
+						.join(" ");
+					return newItem;
+				}
+				return item;
+			});
+		}
+		return config;
+	},
+	(error) => Promise.reject(error)
+);
+
 export { axiosProfile, axiosArchives };
